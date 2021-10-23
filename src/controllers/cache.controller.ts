44757e5d4dda98a12ac55322f918b0a4185faa13
Nacho <nachoborello97@gmail.com>
2021-10-23 14:17:02 -0300

    implemented TTL

--- src/controllers/cache.controller.ts
+++ src/controllers/cache.controller.ts
@@ -1,15 +1,22 @@
 import { createRandomString } from "../utils/string";
-import { Cache, CacheModel } from "../models/cache.model";
+import { CacheModel } from "../models/cache.model";
+import moment from "moment";
 
-
+const TTL = process.env.TTL ?? 60000;
 export class CacheController {
     public static async getByKey(key: string): Promise<string> {
         try {
             const cache = await CacheModel.getByKey(key);
             console.log({cache});
             if(cache) {
-                // TODO: update TTL
-                return cache.value;
+                const is_expired = moment(cache.ttl).isBefore(moment());
+                if(is_expired) {
+                    const updated = await CacheModel.update(key, createRandomString(20));
+                    return updated.value;
+                } else {
+                    await CacheModel.updateTTL(key);
+                    return cache.value;
+                }
             } else {
                 const value = createRandomString(20);
                 const cache = await CacheModel.create(key, value);
--- src/models/cache.model.ts
+++ src/models/cache.model.ts
@@ -1,9 +1,13 @@
+import moment from 'moment';
 import monk, { IMonkManager } from 'monk';
 
+const TTL = process.env.TTL ?? 60000;
+
 export interface Cache {
-    _id: number;
+    _id?: number;
     key: string;
     value: string;
+    ttl: Date;
 }
 
 export class CacheModel {
@@ -42,7 +46,8 @@ export class CacheModel {
         const cache_collection = await this.connect();
 
         try {
-            const cache = await cache_collection.insert({ key, value })
+            const ttl = moment().add(TTL,'milliseconds').toDate();
+            const cache = await cache_collection.insert({ key, value, ttl })
             await this.close();
             return cache;
         } catch (error) {
@@ -52,17 +57,18 @@ export class CacheModel {
         }
     }
 
-    public static async update(key: string, value: string): Promise<any> {
+    public static async update(key: string, value: string): Promise<Cache> {
         const cache_collection = await this.connect();
 
         try {
-            const result = await cache_collection.update({ key }, { $set: { key, value } });
+            const ttl = moment().add(TTL,'milliseconds').toDate();
+            const result = await cache_collection.update({ key }, { $set: { key, value, ttl } });
             await this.close();
             if(result.nModified === 0) {
                 const created = await this.create(key, value);
-                return { key: created.key, value: created.value };
+                return { key: created.key, value: created.value, ttl: created.ttl };
             } else {
-                return { key, value };
+                return { key, value, ttl };
             }
         } catch (error) {
             await this.close();
@@ -100,4 +106,18 @@ export class CacheModel {
             return error;
         }
     }
+    
+    public static async updateTTL(key: string): Promise<void> {
+        const cache_collection = await this.connect();
+
+        try {
+            await cache_collection.update({ key }, { $set: { ttl: moment().add(TTL,'milliseconds').toDate() } })
+            return;
+        } catch (error) {
+            console.log({ error });
+            return error;
+        }
+    }
+
+
 }
